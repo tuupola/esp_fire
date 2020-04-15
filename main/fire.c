5bffd2f74d9e3e733aa9f6d8a04b411a7076b698
Mika Tuupola <tuupola@appelsiini.net>
2020-04-16 00:58:29 +0700

    Import sources from tuupola/esp-examples

---
+++ main/fire.c
@@ -0,0 +1,165 @@
+/*
+
+Copyright (c) 2004-2007 Lode Vandevenne
+Copyright (c) 2018-2020 Mika Tuupola
+
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+    * Redistributions of source code must retain the above copyright notice,
+      this list of conditions and the following disclaimer.
+
+    * Redistributions in binary form must reproduce the above copyright
+      notice, this list of conditions and the following disclaimer in the
+      documentation and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+POSSIBILITY OF SUCH DAMAGE.
+
+-cut-
+Adapted from Lode's classic fire tutorial:
+http://lodev.org/cgtutor/fire.html
+
+SPDX-License-Identifier:: BSD-2-Clause
+
+*/
+
+#include <stdbool.h>
+#include <stdint.h>
+#include <math.h>
+
+#include <string.h>
+#include <stdio.h>
+
+#include <bitmap.h>
+#include <esp_log.h>
+
+#include "hsl.h"
+#include "rgb565.h"
+#include "bitmap.h"
+#include "fire.h"
+
+//static uint16_t palette[256];
+//static uint8_t fire[FIRE_HEIGHT][FIRE_WIDTH];
+
+static uint16_t *palette;
+static uint8_t **fire;
+
+void fire_init()
+{
+    uint16_t color;
+    hsl_t hsl;
+    rgb_t rgb;
+
+    palette = (uint16_t *) malloc(256 * sizeof(uint16_t));
+
+    fire = (uint8_t **)malloc(FIRE_HEIGHT * sizeof(uint8_t *));
+    for (uint16_t i = 0; i < FIRE_HEIGHT; i++) {
+         fire[i] = (uint8_t *)malloc(FIRE_WIDTH * sizeof(uint8_t));
+    }
+
+    /* Generate palette. */
+    for (uint16_t x = 0; x < 256; x++) {
+        /* Hue goes from 0 to 85: red to yellow. */
+        /* Saturation is always the maximum: 255. */
+        /* Lightness is 0..255 for x=0..128, and 255 for x=128..255. */
+        hsl.h = x / 3;
+        hsl.s = 255;
+        hsl.l = min(255, x * 2);
+
+        rgb = hsl_to_rgb888(&hsl);
+
+        color = rgb565(rgb.r, rgb.g, rgb.b);
+        palette[x] = color;
+    }
+
+    fire_clear();
+}
+
+void fire_clear()
+{
+    //memset(fire, 0x00, FIRE_HEIGHT * FIRE_WIDTH);
+    for(uint16_t y = 0; y < FIRE_HEIGHT; y++) {
+        memset(fire[y], 0x00, FIRE_WIDTH);
+        // for(uint16_t x = 0; x < FIRE_WIDTH; x++) {
+        //    fire[y][x] = 0;
+        // }
+    }
+}
+
+void fire_effect(bitmap_t *dst, uint16_t multiplier, uint16_t divider)
+{
+    /* The fire effect itself. */
+    for(uint16_t y = 0; y < FIRE_HEIGHT - 1; y++) {
+        for(uint16_t x = 0; x < FIRE_WIDTH; x++) {
+            fire[y][x] =
+                ((fire[(y + 1) % FIRE_HEIGHT][(x - 1 + FIRE_WIDTH) % FIRE_WIDTH]
+                + fire[(y + 1) % FIRE_HEIGHT][(x) % FIRE_WIDTH]
+                + fire[(y + 1) % FIRE_HEIGHT][(x + 1) % FIRE_WIDTH]
+                + fire[(y + 2) % FIRE_HEIGHT][(x) % FIRE_WIDTH])
+                * multiplier) / divider;
+        }
+    }
+
+    /* Copy everything to the destination bitmap. */
+    for(uint16_t y = 0; y < FIRE_HEIGHT - 1; y++) {
+        for(uint16_t x = 0; x < FIRE_WIDTH; x++) {
+            uint16_t *fireptr = (uint16_t *) (dst->buffer + dst->pitch * y + (dst->depth / 8) * x);
+            *fireptr = palette[fire[y][x]];
+        }
+    }
+}
+
+void fire_feed()
+{
+    /* Random bright line in the bottom. */
+    for (uint16_t x = 0; x < FIRE_WIDTH; x++) {
+        uint8_t color = abs(32768 + rand());
+        fire[FIRE_HEIGHT - 1][x] = color;
+    }
+}
+
+
+void fire_putchar(char ascii, int16_t x0, int16_t y0, const char font[][8])
+{
+    /* Basic clipping. */
+    if ((x0 < 0) || (y0 < 0) || (x0 > FIRE_WIDTH - 8 ) || (y0 > FIRE_HEIGHT - 8)) {
+        return;
+    }
+
+    /* First row is the font settings. */
+    ascii = ascii & 0x7F;
+    ascii = ascii + 1;
+
+    uint8_t color = (rand() % 55) + 100;
+
+    for (uint8_t x = 0; x < 8; x++) {
+        for (uint8_t y = 0; y < 8; y++) {
+            bool set = font[(uint8_t)ascii][y] & 1 << x;
+            if (set) {
+                fire[y0 + y][x0 + x] = color;
+            }
+        }
+    }
+}
+
+void fire_putstring(char ascii[], int16_t x0, int16_t y0, const char font[][8])
+{
+    for (uint8_t i = 0; i < strlen(ascii); i++) {
+        uint8_t amp =  abs(sin(0.035 * x0) * 10);
+
+        fire_putchar(ascii[i], x0, y0 + amp, font);
+        x0 += 7;
+    }
+}
---
+++ main/main.c
@@ -0,0 +1,196 @@
+/*
+
+MIT No Attribution
+
+Copyright (c) 2018-2020 Mika Tuupola
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+-cut-
+
+SPDX-License-Identifier: MIT-0
+
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <freertos/FreeRTOS.h>
+#include <freertos/semphr.h>
+#include <freertos/task.h>
+#include <esp_log.h>
+#include <esp_task_wdt.h>
+#include <bitmap.h>
+#include <copepod_hal.h>
+#include <copepod.h>
+#include <font8x8.h>
+#include <rgb565.h>
+#include <fps.h>
+
+#include "fire.h"
+#include "fps2.h"
+
+static const char *TAG = "main";
+
+static SemaphoreHandle_t mutex;
+static float fb_fps;
+static float fx_fps;
+static uint16_t demo = 0;
+
+/*
+ * Flushes the framebuffer to display in a loop. This demo is
+ * capped to 30 fps.
+ */
+void framebuffer_task(void *params)
+{
+    TickType_t last;
+    const TickType_t frequency = 1000 / 30 / portTICK_RATE_MS;
+
+    last = xTaskGetTickCount();
+
+    while (1) {
+        xSemaphoreTake(mutex, portMAX_DELAY);
+        pod_flush();
+        xSemaphoreGive(mutex);
+        fb_fps = fps();
+        vTaskDelayUntil(&last, frequency);
+    }
+
+    vTaskDelete(NULL);
+}
+
+/*
+ * Displays the info bar on top of the screen. Separate FPS values
+ * for the fire effect and the framebuffer. Effect is actually drawn
+ * faster the display is refreshed.
+ */
+void fps_task(void *params)
+{
+    uint16_t color;
+    char message[42];
+
+    while (1) {
+        color = rgb565(0, 255, 0);
+
+        sprintf(message, "%.*f FXPS    ", 0, fx_fps);
+        pod_put_text(message, 8, 4, color, font8x8);
+        sprintf(message, "%.*f FPS  ", 1, fb_fps);
+        pod_put_text(message, DISPLAY_WIDTH - 72, 4, color, font8x8);
+
+        ESP_LOGI(TAG, "%.*f FXPS / %.*f FPS", 1, fx_fps, 1, fb_fps);
+        vTaskDelay(1000 / portTICK_RATE_MS);
+    }
+    vTaskDelete(NULL);
+}
+
+/*
+ * Animated fire effect. Adapted from http://lodev.org/cgtutor/fire.html.
+ */
+void fire_task(void *params)
+{
+    ESP_LOGI(TAG, "Heap before initialising fire: %d", esp_get_free_heap_size());
+
+    int16_t sx = 100; /* Sine scroller x position. */
+
+    bitmap_t bitmap = {
+        .width = FIRE_WIDTH,
+        .height = FIRE_HEIGHT,
+        .depth = DISPLAY_DEPTH,
+    };
+
+    /* But buffer to heap, there was not enough stack. */
+    uint8_t *buffer = malloc(bitmap_size(&bitmap));
+
+    bitmap_init(&bitmap, buffer);
+    ESP_LOGI(TAG, "Heap after fire bitmap: %d", esp_get_free_heap_size());
+
+    fire_init();
+    ESP_LOGI(TAG, "Heap after fire init: %d", esp_get_free_heap_size());
+
+    while (1) {
+
+        /* Fine sire scroller. */
+        if (0 == demo % 3) {
+            fire_putstring(" IS IT 90'S AGAIN?      HELLO M5STACK!      THANKS LODE...", sx, FIRE_HEIGHT / 2, font8x8);
+
+            sx = sx - 2;
+            if (sx < -440) {
+                sx = FIRE_WIDTH - 8;
+            }
+
+            fire_effect(&bitmap, 30, 130);
+        }
+
+        /* Static M5Stack text. */
+        if (1 == demo % 3) {
+            fire_putchar('M', (FIRE_WIDTH / 2) - 28, (FIRE_HEIGHT / 2) - 2, font8x8);
+            fire_putchar('5', (FIRE_WIDTH / 2) - 20, (FIRE_HEIGHT / 2), font8x8);
+            fire_putchar('S', (FIRE_WIDTH / 2) - 12, (FIRE_HEIGHT / 2) + 2, font8x8);
+            fire_putchar('T', (FIRE_WIDTH / 2) - 4, (FIRE_HEIGHT / 2) + 3, font8x8);
+            fire_putchar('A', (FIRE_WIDTH / 2) + 4, (FIRE_HEIGHT / 2), font8x8);
+            fire_putchar('C', (FIRE_WIDTH / 2) + 12, (FIRE_HEIGHT / 2) - 2, font8x8);
+            fire_putchar('K', (FIRE_WIDTH / 2) + 20, (FIRE_HEIGHT / 2) - 1, font8x8);
+            fire_effect(&bitmap, 30, 130);
+        }
+
+        /* Can't believe it is not fireplace! */
+        if (2 == demo % 3) {
+            fire_feed();
+            fire_effect(&bitmap, 32, 129);
+        }
+
+        /* Blit the fire bitmap scaled up to framebuffer. */
+        pod_scale_blit(0, 20, DISPLAY_WIDTH, DISPLAY_HEIGHT - 20, &bitmap);
+        //esp_task_wdt_reset();
+
+        /* Update the FX fps counter. */
+        fx_fps = fps2();
+    }
+
+    vTaskDelete(NULL);
+}
+
+void switch_task(void *params)
+{
+    while (1) {
+        demo = demo + 1;
+        vTaskDelay(10000 / portTICK_RATE_MS);
+        fire_clear();
+    }
+
+    vTaskDelete(NULL);
+}
+
+void app_main()
+{
+    ESP_LOGI(TAG, "SDK version: %s", esp_get_idf_version());
+    ESP_LOGI(TAG, "Heap when starting: %d", esp_get_free_heap_size());
+
+    pod_init();
+
+    ESP_LOGI(TAG, "Heap after pod init: %d", esp_get_free_heap_size());
+
+    mutex = xSemaphoreCreateMutex();
+
+    if (NULL != mutex) {
+        xTaskCreatePinnedToCore(framebuffer_task, "Framebuffer", 8192, NULL, 1, NULL, 0);
+        xTaskCreatePinnedToCore(fps_task, "FPS", 4096, NULL, 2, NULL, 1);
+        xTaskCreatePinnedToCore(fire_task, "Fire", 8192, NULL, 1, NULL, 1);
+        xTaskCreatePinnedToCore(switch_task, "Switch", 2048, NULL, 1, NULL, 1);
+    } else {
+        ESP_LOGE(TAG, "No mutex?");
+    }
+}
